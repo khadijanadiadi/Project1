9241c5e92d6121c65de3e9eeda7c6d9e31be04a9
khadijanadiadi <63386328+khadijanadiadi@users.noreply.github.com>
2021-07-16 20:21:33 +0530

    Add files via upload

---
+++ CalculateCircleArea.py
@@ -0,0 +1,6 @@
+import math
+radius=float(input("Enter The radius: "))
+pi=22/7
+Area = 2*pi*radius**2
+print("Radius of circle is: ", radius)
+print("Area of the circle with radius", radius, "is: ",Area)
